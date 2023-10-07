1b8b7896075c9fc43adce71c251fead81e0b44f9
David Vega <omar.vegar@cun.edu.co>
2023-10-06 19:38:45 -0500

    Se realiza ajuste para la base de datos

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="inventiolite";
+		$this->user="root";$this->pass="";$this->host="3.87.217.60";$this->ddbb="inventiolite";
 	}
 
 	function connect(){
