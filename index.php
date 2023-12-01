27b21cdd527b7e736dcfa3a6822c3656e1819ced
David Vega <omar.vegar@cun.edu.co>
2023-12-01 15:59:30 -0500

    Ajustes de Pago

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recordatorio de Pago</title>
+</head>
+<body>
 <?php
-/**
-* @author evilnapsis
-**/
+// Verificar si el pago ha sido realizado
+$pagoRealizado = false; // Cambia esto con tu lógica real de verificación de pago
 
-define("ROOT", dirname(__FILE__));
-
-$debug= false;
-if($debug){
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if ($pagoRealizado) {
+    // Si el pago ha sido realizado, redirigir al usuario a la plataforma
+    header("Location: plataforma.php");
+    exit();
 }
-
-include "core/autoload.php";
-
-ob_start();
-session_start();
-Core::$root="";
-
-// si quieres que se muestre las consultas SQL debes decomentar la siguiente linea
-// Core::$debug_sql = true;
-
-$lb = new Lb();
-$lb->start();
-
 ?>
+
+<div>
+    <h1>Recordatorio de Pago</h1>
+    <p>Por favor, realiza el pago para acceder a la plataforma.</p>
+    <p>Una vez que hayas realizado el pago, podrás ingresar y disfrutar de nuestros servicios.</p>
+    <!-- Agrega aquí el enlace o botón para llevar al usuario a la página de pago -->
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
